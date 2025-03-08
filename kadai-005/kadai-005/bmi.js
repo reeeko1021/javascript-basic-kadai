f64b9c116f2a56d14525bf1e064eed3f14fea0d8
reeeko1021 <reee.yuuu17@icloud.com>
2025-03-09 00:35:50 +0900

    再提出です。

--- kadai-005/kadai-005/bmi.js
+++ kadai-005/kadai-005/bmi.js
@@ -1,12 +1,15 @@
 //体重の代入
 let weight = 68;
 
+
 //身長の代入
 let height = 1.7;
 
 //BMIの計算
 let bmi = weight / (height * height);
 
-//各、出力
-console.log('BMI: ' + bmi);
+//出力
+console.log(weight);
+console.log(height);
+console.log(bmi);
 
